9ce03cd707f8167202b30566855ad0c6229f4274
Adrien Navratil <adrien1975@live.fr>
2016-04-09 15:58:16 +0200

    Basic theme system

--- src/main/java/org/libnoctis/components/NComponent.java
+++ src/main/java/org/libnoctis/components/NComponent.java
@@ -1,16 +1,20 @@
 /*
  * Copyright 2015-2016 Adrien "Litarvan" Navratil & Victor "Wytrem"
+ *
  * This file is part of Libnoctis.
+
  * Libnoctis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
  * Libnoctis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Libnoctis. If not, see <http://www.gnu.org/licenses/>.
+ * along with Libnoctis.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.libnoctis.components;
 
