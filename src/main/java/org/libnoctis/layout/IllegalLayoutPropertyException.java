e93f3aafa975549136819f2d39dc5341204f53d6
Adrien Navratil <adrien1975@live.fr>
2016-05-08 20:08:55 +0200

    Absolute layout and new exception for layouts

---
+++ src/main/java/org/libnoctis/layout/IllegalLayoutPropertyException.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015-2016 Adrien "Litarvan" Navratil & Victor "Wytrem"
+ *
+ * This file is part of Libnoctis.
+ *
+ * Libnoctis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libnoctis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Libnoctis. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.libnoctis.layout;
+
+import org.libnoctis.components.NComponent;
+
+/**
+ * The Illegal Layout Property Exception
+ *
+ * <p>
+ *     This exception is thrown when a component hasn't
+ *     the right illegal layout property type.
+ * </p>
+ *
+ * @author Litarvan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class IllegalLayoutPropertyException extends RuntimeException
+{
+    /**
+     * The Illegal Layout Property Exception
+     *
+     * @param component The component that hasn't the right layout property type
+     * @param shouldBe The type of layout property the component should have
+     */
+    public IllegalLayoutPropertyException(NComponent component, String shouldBe)
+    {
+        super("A component has as position a " + (component.getPosition() == null ? "null" :  component.getPosition().getClass().getSimpleName()) + " but should have a " + shouldBe);
+    }
+}
---
+++ src/main/java/org/libnoctis/layout/base/AbsoluteLayout.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015-2016 Adrien "Litarvan" Navratil & Victor "Wytrem"
+ *
+ * This file is part of Libnoctis.
+ *
+ * Libnoctis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libnoctis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Libnoctis. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.libnoctis.layout.base;
+
+import org.libnoctis.components.NComponent;
+import org.libnoctis.layout.IllegalLayoutPropertyException;
+import org.libnoctis.layout.NLayout;
+import org.libnoctis.util.Vector2i;
+import org.libnoctis.util.exception.IllegalNoctisStateException;
+
+/**
+ * The Absolute Layout
+ *
+ * <p>
+ *     This layout just draw components at a given position.
+ * </p>
+ *
+ * @see AbsoluteLayoutProperty
+ *
+ * @author Litarvan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class AbsoluteLayout extends NLayout
+{
+    @Override
+    public Vector2i[] getElementsPosition(NComponent[] components)
+    {
+        Vector2i[] positions = new Vector2i[components.length];
+
+        for (int i = 0; i < positions.length; i++)
+        {
+            NComponent component = components[i];
+
+            if (!(component.getPosition() instanceof AbsoluteLayoutProperty))
+                throw new IllegalLayoutPropertyException(component, "AbsoluteLayoutProperty");
+
+            positions[i] = ((AbsoluteLayoutProperty) component.getPosition()).getPosition();
+        }
+
+        return positions;
+    }
+}
